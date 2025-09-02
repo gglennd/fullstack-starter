24deceab6a565c450ae7ce6742facbcd850a2978
gglennd <grdimaano18@gmail.com>
2025-09-02 14:55:35 +0800

    Added Basci setup of Hono server and Vite React app

---
+++ apps/web/src/routes/__root.tsx
@@ -0,0 +1,44 @@
+/// <reference types="vite/client" />
+import { createRootRoute, HeadContent, Outlet, Scripts } from "@tanstack/react-router";
+import * as React from "react";
+
+export const Route = createRootRoute({
+  head: () => ({
+    meta: [
+      {
+        charSet: "utf-8",
+      },
+      {
+        name: "viewport",
+        content: "width=device-width, initial-scale=1.0",
+      },
+      {
+        title: "gglennd's fullstack starter",
+      },
+    ],
+    links: [],
+  }),
+  component: RootComponent,
+});
+
+function RootComponent() {
+  return (
+    <RootDocument>
+      <Outlet />
+    </RootDocument>
+  );
+}
+
+function RootDocument({ children }: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <html>
+      <head>
+        <HeadContent />
+      </head>
+      <body>
+        {children}
+        <Scripts />
+      </body>
+    </html>
+  );
+}
